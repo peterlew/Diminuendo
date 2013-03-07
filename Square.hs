a47f87ef1a3e21886af4a1139bd586cc0c5fe537
Peter Lewis <lewispossible@gmail.com>
2013-03-07 17:16:18 -0500

    Changed Square back to standard

--- Square.hs
+++ Square.hs
@@ -10,14 +10,18 @@ import Graphics.UI.GLUT
 vertify3 :: [(GLfloat,GLfloat,GLfloat)] -> IO ()
 vertify3 verts = sequence_ $ map (\(a,b,c) -> vertex $ Vertex3 a b c) verts 
 
+{- here's a weird one with fangs
+
 square w = renderPrimitive Quads $ vertify3
   [ (0, 0, 0), (w, -w, 0), (2*w, -w, 0), (3*w, 0, 0),
     (3*w, w, 0), (2*w, 2*w, 0), (w, 2*w, 0), (0, w, 0), (0, 0, 0)]  
 
-{-
+-}
+
+
 square w = renderPrimitive Quads $ vertify3
     [ (0, 0, 0), (w, 0, 0), (w, w, 0), (0, w, 0) ]
--}
+
 
 
 --Peter Lewis, 2012
